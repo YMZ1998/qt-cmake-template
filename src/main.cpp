c64690f4f8bac0de3fea01d91852e82c71fa580c
YMZ <1320439118@qq.com>
2026-09-22 09:09:28 +0800

    update.

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,17 @@
 #include <QApplication>
 #include <QByteArray>
 #include <QCoreApplication>
+#include <QDir>
+#include <QFileInfo>
 #include <QtGlobal>
 
 int main(int argc, char* argv[])
 {
+    // Make plugins discoverable when launched from another working directory.
+    const QString executableDir = QFileInfo(QString::fromLocal8Bit(argv[0])).absolutePath();
+    qputenv("QT_QPA_PLATFORM_PLUGIN_PATH",
+            QDir::toNativeSeparators(executableDir + QStringLiteral("/platforms")).toLocal8Bit());
+
     // Prefer Windows Media Foundation over the legacy DirectShow backend.
     qputenv("QT_MULTIMEDIA_PREFERRED_PLUGINS", QByteArrayLiteral("windowsmediafoundation"));
 
